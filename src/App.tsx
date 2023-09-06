25f10d08429deace2b6d62c7d178fe0dacee4899
Miles <1978176436@qq.com>
2023-09-06 21:55:01 +0200

    模板逻辑精简

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,36 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
+// 有一个状态 type 1 2 3
+// 1 -> h1
+// 2 -> h2
+// 3 -> h3
 
+// 原则: 模板中的逻辑尽量保持精简
+// 复杂的多分枝逻辑， 收敛为一个函数 通过一个专门的函数来写分支逻辑，模板中只负责调用函数即可
 
-//条件渲染
-//技术方案: 三元表达式(常用)  逻辑和运算
+const getHtag = (type: number) =>{
+  if(type === 1){
+    return <h1>this is h1</h1>
+  }
 
-// 1. 三元表达式 - 满足条件才渲染一个span标签
-// 2. &&
+  if(type === 2){
+    return <h2>this is h2</h2>
+  }
 
-const flag = false
+  if(type === 3){
+    return <h3>this is h3</h3>
+  }
+}
+
 function App() {
 
 
   return (
     <div className='App'>
-      {flag ? <span> this is span</span>: null}
-
-      // 前面布尔值为true 后面的span标签才会显示【这个字段需要被删除】
-      {true && <span>this is span</span>}
+      {getHtag(1)}
+      {getHtag(2)}
+      {getHtag(3)}
 
     </div>
   )
